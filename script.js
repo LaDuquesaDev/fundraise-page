ff24b8b6bfc21d9f21ad2119a1d5aca932d104e5
Yulimar Duque <yduquedealba@gmail.com>
2024-10-01 00:27:59 -0500

    landing page implementation

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+function closeGivingSection() {
+    document.getElementById('giving-section').classList.add('hidden');
+}
